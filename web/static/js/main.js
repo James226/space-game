0a712fc59e0e7a332b01d7835c65f7249cd73160
James Parker <james.parker@esendex.com>
2016-05-29 14:10:21 +0100

    Add player character

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -2,6 +2,21 @@ import controls from './controls'
 import ChunkManager from './chunk-manager'
 import PhysicsManager from './physics-manager'
 
+var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+var tagOrComment = new RegExp(
+    '<(?:'
+    // Comment body.
+    + '!--(?:(?:-*[^->])*--+|-?)'
+    // Special "raw text" elements whose content should be elided.
+    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+    + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+    // Regular name
+    + '|/?[a-z]'
+    + tagBody
+    + ')>',
+    'gi');
+
 export class Game {
     scene: THREE.Scene;
     renderer: THREE.WebGLRenderer;
@@ -26,8 +41,8 @@ export class Game {
         this.container.appendChild(this.renderer.domElement);
 
         this.camera = new THREE.PerspectiveCamera(27, window.innerWidth / window.innerHeight, 1, 3500);
-        this.camera.position.z = 10;
-        this.camera.position.y = 4;
+        this.camera.position.z = 60;
+        this.camera.position.y = 25;
         this.camera.position.x = 0;
 
         // this.camera.rotation.x = -0.7;
@@ -45,18 +60,24 @@ export class Game {
 // this.scene.add( light );
 
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshLambertMaterial( {color: 0x0C0C0C} );
-        this.cube = new THREE.Mesh( geometry, material );
-        this.cube.position.x = 10;
-        this.cube.position.y = 20;
-        this.cube.position.z = 10;
-        this.cube.castShadow = true;
+        // var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        // var material = new THREE.MeshLambertMaterial( {color: 0x0C0C0C} );
+        // this.player = new THREE.Mesh( geometry, material );
+        // this.player.position.x = 10;
+        // this.player.position.y = 20;
+        // this.player.position.z = 10;
+        // this.player.castShadow = true;
 
-        this.scene.add( this.cube );
-        this.cube.add(this.camera);
+        this.chunkManager = new ChunkManager(this.scene);
+        this.physicsManager = new PhysicsManager(p => this.chunkManager.voxelActive(p));
 
-        this.camControls = new controls(this.cube);
+        this.player = new THREE.Object3D();
+        this.player.velocity = new THREE.Vector3(0, -1, 0);
+        this.player.position.set(0, 40, 1);
+        this.scene.add(this.player);
+        this.player.add(this.camera);
+
+        this.camControls = new controls(this.player);
         this.camControls.lookSpeed = 0.4;
         this.camControls.movementSpeed = 20;
         this.camControls.noFly = true;
@@ -66,10 +87,12 @@ export class Game {
         this.camControls.verticalMax = 2.0;
         this.camControls.lon = -150;
         this.camControls.lat = 120;
+        this.physicsManager.add(this.player);
 
-        this.chunkManager = new ChunkManager(this.scene);
-        this.physicsManager = new PhysicsManager(p => this.chunkManager.voxelActive(p));
-        this.physicsManager.add(this.cube);
+        var loader = new THREE.JSONLoader();
+		loader.load( "js/models/knight.js", ( geometry, materials ) => {
+			this.createScene(geometry, materials, 0, 0, 0, 0.2);
+		});
 
         document.getElementById("fullscreen").addEventListener("click", e => {
             if (this.container.requestFullscreen) {
@@ -105,8 +128,10 @@ export class Game {
             }
         });
 
+        var element = document.createElement("div");
         this.socket.lobby.on("new_msg", ({message: message}) => {
-            document.getElementById("chatLog").innerHTML += "<br />" + message;
+            element.innerText = message;
+            document.getElementById("chatLog").innerHTML += (document.getElementById("chatLog").innerHTML.length === 0) ? element.innerHTML : "<br />" + element.innerHTML;
             document.getElementById("chatLog").scrollTop = document.getElementById("chatLog").scrollHeight;
         })
 
@@ -132,6 +157,50 @@ export class Game {
         this.render();
     }
 
+    createScene( geometry, materials, x, y, z, s ) {
+        geometry.computeBoundingBox();
+        var bb = geometry.boundingBox;
+        var path = "textures/cube/Park2/";
+        var format = '.jpg';
+        var urls = [
+                path + 'posx' + format, path + 'negx' + format,
+                path + 'posy' + format, path + 'negy' + format,
+                path + 'posz' + format, path + 'negz' + format
+            ];
+        for ( var i = 0; i < materials.length; i ++ ) {
+            var m = materials[ i ];
+            m.skinning = true;
+            m.morphTargets = true;
+            m.specular.setHSL( 0, 0, 0.1 );
+            m.color.setHSL( 0.6, 0, 0.6 );
+        }
+        var playerModel = new THREE.SkinnedMesh( geometry, new THREE.MultiMaterial( materials ) );
+        playerModel.name = "Knight Mesh";
+        playerModel.position.set( x, y - bb.min.y * s, z );
+        playerModel.scale.set( s, s, s );
+        playerModel.rotation.set(0, Math.PI, 0);
+        this.player.add( playerModel );
+        playerModel.castShadow = true;
+        playerModel.receiveShadow = true;
+        var mixer = new THREE.AnimationMixer( playerModel );
+        var bonesClip = geometry.animations[0];
+        var facesClip = THREE.AnimationClip.CreateFromMorphTargetSequence( 'facialExpressions', playerModel.geometry.morphTargets, 3 );
+
+
+        // var action = mixer.clipAction( bonesClip, null );
+		// action.play();
+    }
+
+
+    removeTags(html) {
+      var oldHtml;
+      do {
+        oldHtml = html;
+        html = html.replace(tagOrComment, '');
+      } while (html !== oldHtml);
+      return html.replace(/</g, '&lt;');
+    }
+
     onKeyPress(event) {
         switch(event.keyCode) {
             case 13:
@@ -150,7 +219,8 @@ export class Game {
         requestAnimationFrame(() => this.render());
         var delta = this.clock.getDelta();
         this.chunkManager.update(delta);
-        this.camControls.update(delta);
+        if (this.camControls)
+            this.camControls.update(delta);
         this.physicsManager.update(delta);
         this.renderer.render(this.scene, this.camera);
         this.stats.update();
--- web/static/js/physics-manager.js
+++ web/static/js/physics-manager.js
@@ -10,11 +10,24 @@ class PhysicsManager {
 
     update(deltaTime) {
         for (var i in this.objects) {
+            // if (!this.voxelCollisionCheck(new THREE.Vector3(this.objects[i].position.x, this.objects[i].position.y - 1, this.objects[i].position.z))) {
+            //     this.objects[i].position.y -= 10 * deltaTime;
+            // } else if(this.voxelCollisionCheck(new THREE.Vector3(this.objects[i].position.x, this.objects[i].position.y - 0.9, this.objects[i].position.z))) {
+            //     this.objects[i].position.y += 20 * deltaTime;
+            // }
+            var velocity = new THREE.Vector3(this.objects[i].velocity.x, this.objects[i].velocity.y, this.objects[i].velocity.z)
             if (!this.voxelCollisionCheck(new THREE.Vector3(this.objects[i].position.x, this.objects[i].position.y - 1, this.objects[i].position.z))) {
-                this.objects[i].position.y -= 3 * deltaTime;
-            } else if(this.voxelCollisionCheck(this.objects[i].position)) {
-                this.objects[i].position.y += 1 * deltaTime;
+                velocity.y = -10;
+            } else {
+                var space = this.voxelCollisionCheck(new THREE.Vector3(this.objects[i].position.x, this.objects[i].position.y - 0.5, this.objects[i].position.z));
+                if (!space) {
+                    velocity.y = 0;
+                    this.objects[i].position.y = Math.round(this.objects[i].position.y)  - 0.01;
+                } else {
+                    velocity.y = 20;
+                }
             }
+            this.objects[i].position.add(velocity.multiplyScalar(deltaTime))
         }
     }
 }
